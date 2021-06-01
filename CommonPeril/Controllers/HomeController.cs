b5c5f6282fa2249c17e772cdd29fbde16d702dee
Mark K <mark@commonperil.com>
2021-05-31 20:17:56 -0400

    Initial Repo add

---
+++ CommonPeril/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CommonPeril.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+
+namespace CommonPeril.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+       
+        public HomeController()
+        {
+           
+            
+        }
+        public IActionResult Main()
+        {
+            RestClient client = new RestClient("https://script.google.com/macros/s/AKfycbxTnujwWVsKv2nmfWoAbafq5JFK4EVjVhmigGf0AqPu5anv_0oVo4O2Wa8OBMI-5ey_Mw");
+            var request = new RestRequest("exec", DataFormat.Json);
+            var timeline = client.Execute(request);
+            JObject completeJObject = JObject.Parse(timeline.Content);
+            var results = completeJObject["body"]["dates"].Children().ToList();
+            var stringResults = JsonConvert.SerializeObject(results);
+            var modelReseults = JsonConvert.DeserializeObject<List<ZoomUsageModel>>(stringResults);
+
+            return View(modelReseults);
+        }
+
+        public IActionResult Minor()
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ CommonPeril/Models/ZoomUsageModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CommonPeril.Models
+{
+    public class ZoomUsageModel
+    {
+        [DataType(DataType.Date)]
+        [JsonProperty("date")]
+        public DateTime MeetingDate { get; set; }
+        [JsonProperty("meetings")]
+        public int Meetings { get; set; }
+        [JsonProperty("participants")]
+        public int Users { get; set; }
+        [JsonProperty("meeting_minutes")]
+        public int TotalMinutes { get; set; }
+    }
+}
